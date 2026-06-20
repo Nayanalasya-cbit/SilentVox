262a2f5737ceb3b8eb04b6d850a47fef2d98e3b9
Nayanalasya-cbit <lasyamoluguri08>
2026-06-20 16:41:38 +0530

    Add protected route middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,9 +4,15 @@ const {
   loginUser,
 } = require("../controllers/authController");
 
+const { protect } = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+router.get("/profile", protect, (req, res) => {
+  res.json(req.user);
+});
 
 module.exports = router;
